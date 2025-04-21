8c1db69d89c2cb178a5eedc1b7389173f84dfa65
Likitha177 <likithareddy177@gmail.com>
2025-04-21 13:17:21 +0000

    latest

--- Project/src/test/java/uistore/TonerPageLocator.java
+++ Project/src/test/java/uistore/TonerPageLocator.java
@@ -4,6 +4,6 @@ import org.openqa.selenium.By;
 
 public class TonerPageLocator {
     public static By searchBar=By.cssSelector("input[placeholder='Search on Nykaa']");
-    public static By toner=By.cssSelector("#title .css-c0pzm0");
-    public static By sortBy=By.cssSelector(".css-1aucgde");
+    public static By toner=By.id("title .css-c0pzm0");
+    public static By sortBy=By.className("css-1aucgde");
 }
