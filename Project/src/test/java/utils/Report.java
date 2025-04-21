65647c568b89a7182eecff926edcd7ce5e1c2d0a
Likitha177 <likithareddy177@gmail.com>
2025-04-21 12:36:13 +0000

    reporter

--- Project/src/test/java/utils/Report.java
+++ Project/src/test/java/utils/Report.java
@@ -1,5 +1,13 @@
 package utils;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
@@ -14,4 +22,17 @@ public class Report {
         reports.attachReporter(sparkReporter);
         return reports;
     }
+    public static void addSCreenshotToReport(ExtentTest test, WebDriver driver, String message) {
+        try {
+            File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String screenshotPath = System.getProperty("user.dir") + "/screenshots/" + ".png";
+            Files.createDirectories(Paths.get(System.getProperty("user.dir") + "/screenshots/"));
+            File destFile = new File(screenshotPath);
+            Files.copy(srcFile.toPath(), destFile.toPath());
+            test.addScreenCaptureFromPath(screenshotPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+            test.fail("Failed to capture screenshot: " + e.getMessage());
+        }
+}
 }
