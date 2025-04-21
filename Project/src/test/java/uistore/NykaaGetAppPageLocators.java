72f967c63f71010d569511004665be8388ef3a1c
Tanushree227 <tanushreedas813@gmail.com>
2025-04-21 12:53:55 +0000

    add new locator file

--- Project/src/test/java/uistore/NykaaGetAppPageLocators.java
+++ Project/src/test/java/uistore/NykaaGetAppPageLocators.java
@@ -3,5 +3,8 @@ package uistore;
 import org.openqa.selenium.By;
 
 public class NykaaGetAppPageLocators {
-    public static By getApp = By.cssSelector("");
+    public static By getApp = By.xpath("//span[text() = 'Get App']");
+    public static By mainHeading = By.className("main-heading");
+    public static By mobileInputField = By.id("mob");
+    public static By termsConditions = By.cssSelector("[href='/policy#terms-conditions']");
 }
