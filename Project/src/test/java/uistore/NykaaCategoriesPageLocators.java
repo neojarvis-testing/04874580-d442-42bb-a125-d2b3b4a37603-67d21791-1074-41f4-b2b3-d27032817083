6b90e98c17c5d4ab8488c62833daa08c184f1077
Tanushree227 <tanushreedas813@gmail.com>
2025-04-21 15:28:23 +0000

    Three locators for now added to java file

---
+++ Project/src/test/java/uistore/NykaaCategoriesPageLocators.java
@@ -0,0 +1,9 @@
+package uistore;
+
+import org.openqa.selenium.By;
+
+public class NykaaCategoriesPageLocators {
+    public static By categories = By.id("category");
+    public static By searchBar = By.name("search-suggestions-nykaa");
+    public static By kajal = By.cssSelector("h1.css-c0pzm0");
+}
