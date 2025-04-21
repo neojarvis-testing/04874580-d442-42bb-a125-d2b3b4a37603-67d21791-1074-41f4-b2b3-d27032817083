c9638127f6b153bb3ad4091e02c56d38b50d83b0
Likitha177 <likithareddy177@gmail.com>
2025-04-21 11:45:31 +0000

    reporter

--- Project/src/test/java/utils/Report.java
+++ Project/src/test/java/utils/Report.java
@@ -1,5 +1,17 @@
 package utils;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class Report {
-    
+    public static ExtentTest test;
+    public static ExtentReports reports;
+
+    public static ExtentReports generateExtenteReports(String reportName){
+        ExtentSparkReporter sparkReporter= new ExtentSparkReporter(System.getProperty("user.dir")+"/reports"+reportName+".html");
+        reports= new ExtentReports();
+        reports.attachReporter(sparkReporter);
+        return reports;
+    }
 }
