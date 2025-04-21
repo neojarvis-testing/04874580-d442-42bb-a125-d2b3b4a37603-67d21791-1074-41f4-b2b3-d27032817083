8857268314698638fefc3485dcd2170218728759
Rahulsavala <rahulshavala@gmail.com>
2025-04-21 14:31:51 +0000

    added

--- Project/src/test/java/uistore/NykaaSearchForPillowLocators.java
+++ Project/src/test/java/uistore/NykaaSearchForPillowLocators.java
@@ -15,5 +15,5 @@ public class NykaaSearchForPillowLocators {
     public static By firstProduct = By.cssSelector("a[href='/sleepsia-full-body-pillow-ultra-soft-fiber-bed-pillows-for-side-and-back-sleepers/p/7745834?productId=7745834&pps=1']");
     public static By addToBag = By.cssSelector("div[class='css-vp18r8']>button[type='button']");
     public static By cart = By.cssSelector("button[id='header-bag-icon']");
-    public static By bag = By.cssSelector("div[class='mob-header-layout css-e2nzen esabwed0']");
+    public static By bag = By.cssSelector("p[class='css-1v1x2g9 e171rb9k0']");
 }
