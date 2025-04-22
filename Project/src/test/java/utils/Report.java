8a96d97b9948e529e8db4a74ce20bc162a4d507f
Rahulsavala <rahulshavala@gmail.com>
2025-04-22 13:36:13 +0000

    message

--- Project/src/test/java/utils/Report.java
+++ Project/src/test/java/utils/Report.java
@@ -1,67 +1,65 @@
 package utils;
  
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
  
+import java.io.File;
+import java.util.Base64;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
- 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
-import com.google.common.io.Files;
  
-public class Report {
-	public static FileInputStream fip;
-    public static ExtentReports report;
-    public static ExtentTest test;
-    public static ExtentSparkReporter spark;
-    public static TakesScreenshot ts;
+public class Report extends Base {
+    private static ExtentReports extentReport;
+    private static ExtentSparkReporter extentSparkReporter;
  
-    public static ExtentReports generateReport(String name) {
-        String path = System.getProperty("user.dir")+"/reports/"+name+".html";
-        File file = new File(path);
- 
-        report = new ExtentReports();
-        spark = new ExtentSparkReporter(file);
-        spark.config().setTheme(Theme.DARK);
-        report.attachReporter(spark);
-        return report;
+    public static ExtentReports generateReport() {
+        return generateReport("TEST REPORT");
     }
-	public static String captureScreenShot(String filename) {
-        try {
-            String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-            String name = filename + timestamp + ".png";
-            String destPath =  "./"+name;
- 
-			ts = (TakesScreenshot)Base.driver;
-            File file = ts.getScreenshotAs(OutputType.FILE);
- 
-            File screenDir = new File(System.getProperty("user.dir") + "/reports/screenShots");          
-            if (!screenDir.exists()) {
-                screenDir.mkdirs();
+    public static ExtentReports generateReport(String reportName) {
+        if (extentReport == null) {
+            extentReport = createExtentReport(reportName);
+        }
+        return extentReport;
+    }
+    private static ExtentReports createExtentReport(String reportName) {
+        if (extentReport == null) {
+            String reportsPath = System.getProperty("user.dir") + "/reports/";
+            String report = reportsPath + reportName + ".html"; // Correct path
+            File reportsDir = new File(reportsPath);
+            if (!reportsDir.exists()) {
+                reportsDir.mkdirs();
             }
-		    File target = new File(screenDir, name);
- 
-			Files.copy(file, target);
-            return destPath;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        return "";
-	}
- 
-    public static void attachScreenshotToReport(String filename, ExtentTest test, String description){
+            extentSparkReporter = new ExtentSparkReporter(report); // Use correct file path
+            extentReport = new ExtentReports();
+            extentReport.attachReporter(extentSparkReporter);
+        }
+        return extentReport;
+    }
+    public static String takeScreenshotAsBase64() {
+        String base64String = "";
         try {
-            test.log(Status.INFO, description, MediaEntityBuilder.createScreenCaptureFromPath(captureScreenShot(filename)).build());
+            File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            byte[] fileContent = FileUtils.readFileToByteArray(screenshotFile);
+            base64String = Base64.getEncoder().encodeToString(fileContent);
         } catch (Exception e) {
             e.printStackTrace();
         }
-	}
+        return base64String;
+    }
+    public static void attachScreenshotToReport(String reportName, ExtentTest test, String description) {
+        try {
+            String base64Screenshot = takeScreenshotAsBase64();
+            if (!base64Screenshot.isEmpty()) {
+                test.info(description, MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
+            } else {
+                test.log(Status.FAIL, "Screenshot capture failed.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
